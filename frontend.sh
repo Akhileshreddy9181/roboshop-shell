e0e0a5c420e8ce0c1616c7c169d730b451eb13b6
akhil <akhileshreddy9181@gmail.com>
2023-02-24 18:55:21 -0500

    Added conf script in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
-code_dir=${pwd}
+code_dir=$(pwd)
 yum install nginx -y
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
@@ -6,5 +6,5 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
-systemctl start nginx
+systemctl restart nginx
 
