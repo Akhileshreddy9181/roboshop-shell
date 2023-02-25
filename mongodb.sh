9af16b4917d2a6bcd6649bba0ba309534ba3cb7f
akhil <akhileshreddy9181@gmail.com>
2023-02-24 22:06:17 -0500

    Added conf script in frontend.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,16 +1,16 @@
 source common.sh
 
 print_head "Set up MongoDB repo"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "Install MongoDB"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log_file}
 
 print_head "Updating the mongodb listen address "
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${log_file}
 
 print_head "Enable MongoDB"
-systemctl enable mongod
+systemctl enable mongod &>>${log_file}
 
 print_head "Start MongoDB"
-systemctl restart mongod
+systemctl restart mongod &>>${log_file}
