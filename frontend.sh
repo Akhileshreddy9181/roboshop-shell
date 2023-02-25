fa76aa70a7d02576075547d50370dd7b117cb791
akhil <akhileshreddy9181@gmail.com>
2023-02-24 21:03:33 -0500

    Added conf script in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,5 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
-
-print_head()
-{
-  echo -e "\e[34m$1\e[0m"
-  }
-
+#loading the common script
+source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
--- mongodb.sh
+++ mongodb.sh
@@ -1,1 +1,20 @@
-vim /etc/yum.repos.d/mongo.repo
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+
+print_head()
+{
+  echo -e "\e[34m$1\e[0m"
+  }
+
+print_head "Set up MongoDB repo"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install MongoDB"
+yum install mongodb-org -y
+
+print_head "Enable MongoDB"
+systemctl enable mongod
+
+print_head "Start MongoDB"
+systemctl start mongod
