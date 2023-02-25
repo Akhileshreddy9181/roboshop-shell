74d37057d85469a3a2cb67ddf5ab90be81895dd9
akhil <akhileshreddy9181@gmail.com>
2023-02-24 21:05:09 -0500

    Added conf script in frontend.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,4 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
-
-print_head()
-{
-  echo -e "\e[34m$1\e[0m"
-  }
+source common.sh
 
 print_head "Set up MongoDB repo"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo
