a68aeb5a55229d190c5ae323508dba5f3eb9ca92
akhil <akhileshreddy9181@gmail.com>
2023-02-24 19:55:03 -0500

    Added conf script in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,33 @@
 code_dir=$(pwd)
+log_file=$(/tmp/roboshop.log)
 
 echo -e "\e[35mInstalling nginx\e[0m"
-yum install nginx -y
+yum install nginx -y &>>${log_file}
 
 
 echo -e "\e[35mRemoving nginx default web content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 
 
 echo -e "\e[35mDownloading front-end code to temp dir\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 cd /usr/share/nginx/html
 
 
 echo -e "\e[35mExtracting the zipped code\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log_file}
 
 
 echo -e "\e[35mCopying nginx conf file\e[0m"
-cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
 
 
 echo -e "\e[35mENabling and Restarting the nginx\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>${log_file}
+systemctl restart nginx &>>${log_file}
 
 echo -e "\e[35mStatus of nginx service\e[0m"
-systemctl status nginx
+systemctl status nginx &>>${log_file}
 
 
 
