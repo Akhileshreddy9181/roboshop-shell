73d0b371c9fabb81da090ffcc27a973b39324368
akhil <akhileshreddy9181@gmail.com>
2023-02-27 19:53:05 -0500

    Added Exit Status to script

--- redis.sh
+++ redis.sh
@@ -12,15 +12,15 @@ print_head "Installing Redis Application"
 yum install redis -y &>>${log_file}
 print_status $?
 
-print_status "Changing the default listen address to universal ip address"
+print_head "Changing the default listen address to universal ip address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
 print_status $?
 
-print_status "Enabling the Redis service"
+print_head "Enabling the Redis service"
 systemctl enable redis &>>${log_file}
 print_status $?
 
 
-print_status "Starting the Redis service"
+print_head "Starting the Redis service"
 systemctl restart redis &>>${log_file}
 print_status $?
