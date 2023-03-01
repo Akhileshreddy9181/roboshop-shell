5d7b8e995d428370da823d282df21cf08e552dfb
akhil <akhileshreddy9181@gmail.com>
2023-02-28 19:15:26 -0500

    Added Exit Status to script

--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,7 @@ source common.sh
 
 mysql_root_passwd=$1
 
-if [ "${mysql_root_passwd}" == "mysql" ]; then
+if [ -z "${mysql_root_passwd}" ]; then
   echo -e "\e[31mMissing MySQL root Password Argument\e[0m"
   exit 1
     fi
